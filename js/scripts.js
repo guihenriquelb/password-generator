40ef5c8f5e60bfddcb85369ef4209f6c8bd3f9d8
Guilherme Henrique <guihenrique011@hotmail.com>
2023-09-27 20:56:06 -0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// selecionar elementos
+const generatePasswordButton = document.querySelector("#generate-password")
+const generatedPasswordButton = document.querySelector("#generated-password")
+
+// funções
+
+// eventos
+generatePasswordButton.addEventListener("click", () => {
+    console.log('teste')
+})
